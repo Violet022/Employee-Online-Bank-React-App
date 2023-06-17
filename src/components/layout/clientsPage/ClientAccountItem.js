e99cae07b666539315987f392339a4fea66635e0
Violet022 <vita.verkhulevskaya@mail.ru>
2023-06-17 16:04:49 +0700

    Обновление содержимого проекта

--- src/components/layout/clientsPage/ClientAccountItem.js
+++ src/components/layout/clientsPage/ClientAccountItem.js
@@ -5,12 +5,12 @@ import { ListGroup, Container, Row, Col, Nav} from 'react-bootstrap';
 function ClientAccountItem(props) {
     return (
         <>
-            <Nav.Link as={Link} to={`/account/${props.account.accountNumber}/1/${props.userId}`}>
+            <Nav.Link as={Link} to={`/account/${props.account.accountNumber}/${props.userId}`}>
                 <ListGroup.Item className="px-6 py-3">
                     <Container>
                         <Row className="justify-content-between">
-                            <Col xs="auto">Номер счета: {props.account.accountNumber}</Col>
-                            <Col xs="auto">
+                            <Col >Номер счета: {props.account.accountNumber}</Col>
+                            <Col>
                                 {
                                     props.account.type === 0
                                         ? <span>Дебетовый счет</span>
@@ -18,7 +18,7 @@ function ClientAccountItem(props) {
 
                                 }
                             </Col>
-                            <Col xs="auto">
+                            <Col>
                                 {
                                     props.account.state === 0
                                         ? <span>Открыт</span>
@@ -26,7 +26,7 @@ function ClientAccountItem(props) {
 
                                 }
                             </Col>
-                            <Col xs="auto">Баланс: {props.account.balance}</Col>
+                            <Col className="text-center">Баланс: {props.account.balance}</Col>
                         </Row>
                     </Container>
                 </ListGroup.Item>
